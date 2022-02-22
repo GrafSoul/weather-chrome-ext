067c9e1bfc1946491447e5b5ac74a1872fe483a9
GrafSoul <grafsoul@gmail.com>
2022-02-22 16:00:33 +0500

    feat: Added api openweathermap.org

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./popup.css";
 
+import { fetchOpenWeatherData } from "../utils/api";
+
 const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData("Tashkent")
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <img src="icon.png" />
